646058c62b8e4d7d1b5c72130ab78b65137e44e4
Valensia1226 <valensia16@yandex.ru>
2023-03-23 20:32:19 +0300

    Finished the code work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"123", "Denmark", "-2", "computer science"};
+Console.Write("[ ");
+for (int i=0; i< array.Length; i++)
+	Console.Write($"{array[i]} ");
+Console.Write("] -> [ ");
+
+for (int i=0; i< array.Length; i++)
+	if (array[i].Length <= 3) Console.Write($"{array[i]} ");
+Console.Write("]");
