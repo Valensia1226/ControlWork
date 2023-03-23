3cd0d833ef71d6cc738a3ec4f98295c1a614ea8b
Valensia1226 <valensia16@yandex.ru>
2023-03-23 20:52:51 +0300

    Redacted the block diagram and the code and start writing the README file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ for (int i=0; i< array.Length; i++)
 	Console.Write($"{array[i]} ");
 Console.Write("] -> [ ");
 
+string[] secondArray = new string[array.Length];
 for (int i=0; i< array.Length; i++)
-	if (array[i].Length <= 3) Console.Write($"{array[i]} ");
+    if (array[i].Length <= 3) 
+    {
+        secondArray[i] = array[i];
+        Console.Write($"{secondArray[i]} ");
+    }
+
 Console.Write("]");
